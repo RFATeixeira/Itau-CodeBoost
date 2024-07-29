d6c24241ea33195200652fd987a7e4ced289cf54
RFATeixeira <RCSTeixeira@hotmail.com>
2024-07-29 14:34:03 -0300

    primeiro commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-blue": "#106EB0",
+        "primary-orange": "#EC7000",
+        "primary-gray": "#33303E",
+        "second-gray": "#4E4B59",
+        "gray-phone": "#F4F4F4",
+        "txt-gray": "#7A7786",
+        "opacity-gray": "(rgba(100, 80, 57, 0.1)",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        img_bg_hero: "url('../assets/bg-hero.jpg')",
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
 export default config;
